1ac062bcdda2d3f40d31c1e6dca40814f96f2274
Crowdbotics <team@crowdbotics.com>
2021-01-14 21:54:12 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -5,7 +5,6 @@ export const installed_blueprints = [
 { name: 'Settings193358', human_name: 'Settings', access_route: 'Settings193358'},
 { name: 'Settings193356', human_name: 'Settings', access_route: 'Settings193356'},
 { name: 'SignIn2193354', human_name: 'Sign In 2', access_route: 'SignIn2193354'},
-{ name: 'BlankScreen0193352', human_name: 'Blank Screen 0', access_route: 'BlankScreen0193352'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -10,7 +10,6 @@ import UserProfile193359Navigator from '../features/UserProfile193359/navigator'
 import Settings193358Navigator from '../features/Settings193358/navigator';
 import Settings193356Navigator from '../features/Settings193356/navigator';
 import SignIn2193354Navigator from '../features/SignIn2193354/navigator';
-import BlankScreen0193352Navigator from '../features/BlankScreen0193352/navigator';
 
 /**
  * new navigators can be imported here
@@ -24,7 +23,6 @@ UserProfile193359: { screen: UserProfile193359Navigator },
 Settings193358: { screen: Settings193358Navigator },
 Settings193356: { screen: Settings193356Navigator },
 SignIn2193354: { screen: SignIn2193354Navigator },
-BlankScreen0193352: { screen: BlankScreen0193352Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
